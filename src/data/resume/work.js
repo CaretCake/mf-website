cb02ef3f93ea189b2122215b2569b3a4f36213e5
Melissa Farrell <caretcake@outlook.com>
2021-01-27 19:17:21 -0500

    Added work data file

---
+++ src/data/resume/work.js
@@ -0,0 +1,55 @@
+const positions = [
+  {
+    company: 'IBM',
+    position: 'zManage Software Performance Engineer',
+    link: '',
+    daterange: 'June 2019 - Present',
+    points: [
+      '',
+      '',
+      '',
+    ],
+  },
+  {
+    company: 'IBM',
+    position: 'Software Performance Engineer Intern',
+    link: '',
+    daterange: 'May 2018 - May 2019',
+    points: [
+      '',
+      '',
+    ],
+  },
+  {
+    company: 'Northern Michigan University',
+    position: 'Teaching Assistant',
+    link: '',
+    daterange: 'January 2019 - May 2019',
+    points: [
+      '',
+      '',
+    ],
+  },
+  {
+    company: 'NMU Lydia M. Olson Library',
+    position: 'Web Developer',
+    link: '',
+    daterange: 'February 2018 - May 2018',
+    points: [
+      '',
+      '',
+    ],
+  },
+  {
+    company: 'Caret Designs',
+    position: 'Graphic Designer / Owner',
+    link: '',
+    daterange: 'July 2017 - July 2018',
+    points: [
+      '',
+      '',
+    ],
+  },
+];
+
+export default positions;
